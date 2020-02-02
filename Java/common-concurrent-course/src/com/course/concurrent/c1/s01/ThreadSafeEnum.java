51205fd763d47782f9e909427b52fc5c404ce060
Mac <mac@MacdeiMac.local>
2020-02-02 21:43:36 +0800

    初始化JDK8源码项目

---
+++ Java/common-concurrent-course/src/com/course/concurrent/c1/s01/ThreadSafeEnum.java
@@ -0,0 +1,12 @@
+package com.course.concurrent.c1.s01;
+
+/**
+ * Created by zhangyuxuan on 2020/2/1
+ */
+public enum ThreadSafeEnum {
+    /*
+    *  thread safe enum
+    * */
+    ImmutableClass,
+    MutableClass;
+}
